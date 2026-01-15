417c2c3e7b59880cf921ed9ce13c2eec036da372
jihu9001 <jihu9001@163.com>
2026-01-15 15:28:46 +0000

    Fix: simplify theme by removing custom VPApp component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import VPApp from './VPApp.vue'
-import { h } from 'vue'
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(VPApp, null, DefaultTheme.Layout.slots)
-  },
-  enhanceApp({ app }) {
-    app.component('VPApp', VPApp)
-  }
+  extends: DefaultTheme
 }
